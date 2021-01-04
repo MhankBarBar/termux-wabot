772435d9d9ec8087f108cad4e4cf9820286453f0
Senpai778 <76933768+Senpai778@users.noreply.github.com>
2021-01-04 16:39:13 -0300

    index.js

--- index.js
+++ index.js
@@ -132,7 +132,7 @@ async function starts() {
 			}
 
 			const botNumber = client.user.jid
-			const ownerNumber = ["6285892766102@s.whatsapp.net"] // replace this with your number
+			const ownerNumber = ["5511950438690@s.whatsapp.net"] // replace this with your number
 			const isGroup = from.endsWith('@g.us')
 			const sender = isGroup ? mek.participant : mek.key.remoteJid
 			const groupMetadata = isGroup ? await client.groupMetadata(from) : ''
